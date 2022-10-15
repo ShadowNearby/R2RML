b958def5f4710afb9ce98fd21aadf499233efbd4
“yanjs git config --global user.name “yanjs <92451186+ShadowNearby@users.noreply.github.com>
2022-10-15 18:58:49 +0800

    pair_hash修正 & format

--- main.cpp
+++ main.cpp
@@ -3,11 +3,24 @@
 //
 
 #include "test/test.h"
+#include "Database.h"
+#include "parser/RDFParser.h"
 
 int main()
 {
+    auto _100 = R"(..\tiny_example)";
     auto _10k = R"(..\example)";
 //    auto _10m = R"(D:\Download\Claros)";
-    RunTimeTest test(_10k);
+//    RunTimeTest test(_10k);
+    std::fstream f;
+    f.open(_100, std::ios::in);
+    RDFParser rdfParser(f);
+    std::vector<Triple> vec;
+    rdfParser.parseFile(vec);
+    Database database;
+    auto triple = &vec[1];
+    auto flag = database.handleTriple(triple);
+    std::cout << flag << std::endl;
     return 0;
+
 }
--- util/Utility.h
+++ util/Utility.h
@@ -15,3 +15,4 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <utility>
